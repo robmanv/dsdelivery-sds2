908c180bcf1a080abb68de89f47c00978a74243e
Robson Vellasques <robson.vellasques@gmail.com>
2021-01-06 22:46:53 -0300

    Add homepage

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css';
 import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <>
+        <Navbar />
+        <Home />
+      </>
     </div>
   );
 }
